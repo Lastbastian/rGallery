dd1eefeb7e5970d3be97802a9a5003a944eb1a47
Chris Bastian <peacethrubeats@gmail.com>
2015-06-15 20:23:02 -0700

    css updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615012716) do
+ActiveRecord::Schema.define(version: 20150616003000) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "image"
+    t.string   "image_title"
+    t.string   "image_file_size"
+    t.string   "image_content_type"
+    t.string   "image_description"
+    t.integer  "category_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
