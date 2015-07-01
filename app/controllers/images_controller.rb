39c110178d3b52292dde97c08437fd66bb3d6368
Chris Bastian <peacethrubeats@gmail.com>
2015-07-01 16:41:14 -0700

    image uploader

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,18 @@
 class ImagesController < ApplicationController
   def new
+    @image = Image.new
   end
 
   def create
+    @image = Image.new(image_params)
+
+    if @image.new
+      flash[:notice] = "Image Created"
+
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
@@ -10,4 +20,10 @@ class ImagesController < ApplicationController
 
   def index
   end
+
+  private
+    def image_params
+      params.require(:image).require(:image, :category_id, :image_title, :image_description, :image_file_size, :iamge_content_type, :remote_image_url)
+    end
+
 end
